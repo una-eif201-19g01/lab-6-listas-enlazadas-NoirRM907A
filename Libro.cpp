0f0a0a11f8de528ad5a088141fba4cebdb1f93c4
Noel Muñoz <estebannoel98@hotmail.com>
2019-09-17 09:27:49 -0600

    commit 1

--- Libro.cpp
+++ Libro.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "Libro.h"
+#include<sstream>
+
+Libro::Libro() {}
+Libro::Libro(int id, std::string nombre, std::string autor):id(id),nombre(nombre), autor(autor){}
+Libro::~Libro(){}
+
+//SET Y GET ID
+void Libro::setID(int id) {
+	Libro::id = id;
+}
+int Libro::getID() const {
+	return id;
+}
+
+//SET Y GET NOMBRE
+void Libro::setNombre(std::string nombre) {
+	Libro::nombre = nombre;
+}
+std::string Libro::getNombre() const {
+	return nombre;
+}
+
+//SET Y GET AUTOR
+void Libro::setAutor(std::string autor){
+	Libro::autor = autor;
+}
+std::string Libro::getAutor() const{
+	return autor;
+}
+
+std::string toString() {
+	std::string portada;
+	portada = "ID: " + std::to_string(id) + "Nombre: " + nombre + "Autor: " + autor + "\n";
+	return portada
+}
--- Libro.h
+++ Libro.h
@@ -4,9 +4,34 @@
 
 #ifndef LAB_6_LISTAS_ENLAZADAS_LIBRO_H
 #define LAB_6_LISTAS_ENLAZADAS_LIBRO_H
-
+#include<iostream>
 
 class Libro {
+private:
+	int id;
+	std::string nombre;
+	std::string autor;
+public:
+	Libro();
+	Libro(int, std::string, std::string);
+	virtual ~Libro();
+
+	//SET Y GET ID
+	void setID(int id);
+	int getID() const;
+
+	//SET Y GET NOMBRE
+	void setNombre(std::string nombre);
+	std::string getNombre() const;
+
+	//SET Y GET AUTOR
+	void setAutor(std::string autor);
+	std::string getAutor() const;
+
+	//toString
+	std::string toString();
+
+
 
 };
 

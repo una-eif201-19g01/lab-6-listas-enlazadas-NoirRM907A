a95908ede4b9310080377e9c98b8e41a4e54f929
NoirRM907A <53484018+NoirRM907A@users.noreply.github.com>
2019-09-29 12:30:47 -0600

    cambios listaLibro(.h)

--- ListaLibros.h
+++ ListaLibros.h
@@ -4,9 +4,34 @@
 
 #ifndef LAB_6_LISTAS_ENLAZADAS_LISTALIBROS_H
 #define LAB_6_LISTAS_ENLAZADAS_LISTALIBROS_H
-
+#include"Nodo.h"
 
 class ListaLibros {
+private:
+	Nodo* primero;
+	Nodo* actual;
+
+public:
+	virtual ListaLibros();
+	virtual ListaLibros();
+	virtual ~ListaLibros();
+
+	//Metodos SET/GET
+	void setPrimero(Nodo*);
+	Nodo* getPrimero();
+
+	void setActual(Nodo*);
+	Nodo* getActual();
+
+	void insertarFinal();
+	bool eliminarFinal();
+	bool encontrarLibro();
+
+
+
+
+
+
 
 };
 
